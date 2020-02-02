c39398df577f944345781ee84d4f8c42b23dcb71
Enes Gezici <enesgezici@hotmail.com>
2020-02-02 21:34:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCore_ConsoleApp_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
